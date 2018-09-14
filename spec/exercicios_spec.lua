340c7fe7b5751071711a08a63e176b4b7c695547
Yan Minari <yangm97@gmail.com>
2018-09-14 01:40:43 -0300

    Add tests for the result_msg function
    Signed-off-by: Yan Minari <yangm97@gmail.com>

--- spec/exercicios_spec.lua
+++ spec/exercicios_spec.lua
@@ -1,5 +1,27 @@
 _G.TEST = true
--- local exercicios = require "exercicios"
--- local u = exercicios.u
+local exercicios = require "exercicios"
+local u = exercicios.u
 -- local ex = exercicios.ex
+describe("User interaction", function()
+	describe("Number input message", function()
+		it("should be different for the first input", function()
+			local fake_output = assert(io.tmpfile("w+"))
+			io.output(fake_output)
+			fake_output:seek("set", 0)
+			u.insert_msg(1)
+			fake_output:seek("set", 0)
+			local result = fake_output:read()
+			assert.equals("Insira o primeiro número", result)
+		end)
+		it("should be generic in any other case", function()
+			local fake_output = assert(io.tmpfile("w+"))
+			io.output(fake_output)
+			fake_output:seek("set", 0)
+			u.insert_msg(2)
+			fake_output:seek("set", 0)
+			local result = fake_output:read()
+			assert.equals("Insira o próximo número", result)
+		end)
+	end)
+end)
 
