711d93a9fda54f1fa6fb70b708d4842389a4b4bf
Yan Minari <yangm97@gmail.com>
2018-09-15 03:50:43 -0300

    Remove code duplication from tests
    Signed-off-by: Yan Minari <yangm97@gmail.com>

--- spec/exercicios_spec.lua
+++ spec/exercicios_spec.lua
@@ -2,24 +2,23 @@ _G.TEST = true
 local exercicios = require "exercicios"
 local u = exercicios.u
 -- local ex = exercicios.ex
+
+local function test_single_output(f, ...)
+	local fake_output = assert(io.tmpfile("w+"))
+	io.output(fake_output)
+	f(...)
+	fake_output:seek("set", 0)
+	return fake_output:read()
+end
+
 describe("User interaction", function()
 	describe("Number input message", function()
 		it("should be different for the first input", function()
-			local fake_output = assert(io.tmpfile("w+"))
-			io.output(fake_output)
-			fake_output:seek("set", 0)
-			u.insert_msg(1)
-			fake_output:seek("set", 0)
-			local result = fake_output:read()
+			local result = test_single_output(u.insert_msg, 1)
 			assert.equals("Insira o primeiro número", result)
 		end)
 		it("should be generic in any other case", function()
-			local fake_output = assert(io.tmpfile("w+"))
-			io.output(fake_output)
-			fake_output:seek("set", 0)
-			u.insert_msg(2)
-			fake_output:seek("set", 0)
-			local result = fake_output:read()
+			local result = test_single_output(u.insert_msg, 2)
 			assert.equals("Insira o próximo número", result)
 		end)
 	end)
