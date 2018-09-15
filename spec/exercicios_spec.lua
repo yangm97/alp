b52fe4574f8b9adfdd54a567aefef0e81b3bf601
Yan Minari <yangm97@gmail.com>
2018-09-15 15:39:20 -0300

    Test whole output just in case stuff slip after newlines
    Signed-off-by: Yan Minari <yangm97@gmail.com>

--- spec/exercicios_spec.lua
+++ spec/exercicios_spec.lua
@@ -3,24 +3,24 @@ local exercicios = require "exercicios"
 local u = exercicios.u
 -- local ex = exercicios.ex
 
-local function test_single_output(f, ...)
+local function test_whole_output(f, ...)
    local fake_output = assert(io.tmpfile("w+"))
    io.output(fake_output)
    f(...)
    fake_output:seek("set", 0)
-   return fake_output:read()
+   return fake_output:read("*all")
 end
 
 describe("User interaction", function()
    describe("display message", function()
       describe("number input", function()
          it("should be different for the first input", function()
-            local result = test_single_output(u.insert_msg, 1)
-            assert.equals("Insira o primeiro número", result)
+            local result = test_whole_output(u.insert_msg, 1)
+            assert.equals("Insira o primeiro número\n", result)
          end)
          it("should be generic in any other case", function()
-            local result = test_single_output(u.insert_msg, 2)
-            assert.equals("Insira o próximo número", result)
+            local result = test_whole_output(u.insert_msg, 2)
+            assert.equals("Insira o próximo número\n", result)
          end)
       end)
       describe("result", function()
