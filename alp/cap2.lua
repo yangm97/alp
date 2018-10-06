aec4671e7be08155afe4babb0fe5032f50b60aa7
Yan Minari <yangm97@gmail.com>
2018-10-06 10:59:40 -0300

    Solve exercises 3, 4, 5 and 6
    Signed-off-by: Yan Minari <yangm97@gmail.com>

--- alp/cap2.lua
+++ alp/cap2.lua
@@ -7,6 +7,28 @@ local exercises = {
 	[2] = function()
 		io.write("Fatec\n")
 	end,
+	[3] = function()
+		local v = utilities.read_multiple_num(2)
+		assert(#v==2, "É preciso dois números para multiplicar.")
+		utilities.result_msg("multiplicação", v[1]*v[2])
+	end,
+	[4] = function()
+      local result, err = utilities.sum(utilities.read_multiple_num(3))
+      utilities.result_msg("soma", result, err)
+	end,
+	[5] = function()
+      local salary = assert(utilities.read_num("Insira o salário atual (R$):"))
+      local rise_p = utilities.percent_to_decimal(assert(utilities.read_num("Insira o aumento (%):")))
+      local rise = salary * rise_p
+
+      io.write("O novo salário é: R$"..(salary+rise))
+      io.write("O aumento foi de: R$"..rise)
+	end,
+	[6] = function()
+		local h = assert(utilities.read_num("Insira a quantidade em horas:"))
+		local m = h * 60
+		io.write("A quantidade em minutos é "..m)
+	end,
 } utilities.set_default(exercises, function()
    io.write("Exercício não encontrado.\n")
 end)
